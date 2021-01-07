c12b67851bea77ec715b724ca0f7f67aaa958d67
jong yoon <dhorlawhddbs@naver.com>
2021-01-07 19:04:34 +0900

    feat : BoardController 클래스 구현
    - URL 맵핑과 BoardService를 통해 처리된 데이터를 뷰에 바인딩하는
    컨트롤러 구현

---
+++ src/main/java/com/jongnan/communityweb/controller/BoardController.java
@@ -0,0 +1,30 @@
+package com.jongnan.communityweb.controller;
+
+import com.jongnan.communityweb.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping({"", "/"})
+    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
+        model.addAttribute("board", boardService.findBoardByIdx(idx));
+        return "/board/form";
+    }
+
+    @GetMapping("/list")
+    public String list(@PageableDefault Pageable pageable, Model model) {
+        model.addAttribute("boardList", boardService.findBoardList(pageable));
+        return "/board/list";
+    }
+}
