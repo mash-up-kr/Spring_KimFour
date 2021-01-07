e1e4db18c173979ba76768659d4477657b950c17
jong yoon <dhorlawhddbs@naver.com>
2021-01-07 18:34:16 +0900

    feat : 프로젝트 생성
    - 간단한 게시판 만들기 시작

---
+++ src/main/java/com/jongnan/communityweb/CommunityWebApplication.java
@@ -0,0 +1,13 @@
+package com.jongnan.communityweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jongnan/communityweb/CommunityWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongnan.communityweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
